4b6a52e38b10cceec42c04df19e61eb3b4f9a5fe
Zhibek <zhibekrahym05@gmail.com>
2022-07-26 12:09:48 +0600

    feat: make ladning responsive

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -15,13 +15,13 @@ export const LandingPage = ({setlevel}) => {
     return (
         <>
         <div className="header-section flex-column">
-            <h1 className="header-text">Learn Code By Practicing <span className="blue-gradient">SwiftUI</span></h1>
+            <h1 className="header-text">Learn Code By Practicing <span className="pink-gradient">SwiftUI</span></h1>
             <h2 className="subhead-text" style={{width: "50%"}}>No more boring courses, videos and articles.
     Learn to code in a revolutionary new, fun, and effective way.</h2>
             <Link to='/play' onClick={onStartHandler}>
                 <button className="play-btn">Get started</button>
             </Link>
-            <div className="example flex-row">
+            <div className="example flex-example">
                 <div className="landing-editor">
                 <pre >
 {`
@@ -36,7 +36,7 @@ export const LandingPage = ({setlevel}) => {
                 autocapitalize="none" 
                 spellcheck="false" 
                 placeholder=".background(.yellow)" 
-                style={{height: "24px", width: "70%", marginLeft: "120px"}} >
+                className="textarea-example">
                 </textarea>
                 <pre>
 {`
@@ -53,29 +53,29 @@ export const LandingPage = ({setlevel}) => {
             </div>
         </div>
             <div className="landing-instructions flex-column">
-                <h1 className="header-text purple-gradient" style={{marginTop: "100px"}}>SwiftUI Adventure</h1>
-                <div className="flex-start-block" style={{marginRight: "500px"}}>
+                <h1 className="header-text pink-gradient" style={{marginTop: "100px"}}>SwiftUI Adventure</h1>
+                <div className="flex-start-block margin-right">
                     <div className="grey-block"></div>
                     <h2 className="desc-text">Read description</h2>
-                    <img src={DescriptionInstruction} alt={"instruction that displays instructions"} style={{width: "600px", height: "548px", borderRadius: "22px"}}/>
+                    <img src={DescriptionInstruction} alt={"instruction that displays instructions"} className="width-half"/>
                 </div>
-                <div className="flex-start-block" style={{marginLeft: "500px"}}>
+                <div className="flex-start-block margin-left">
                     <div className="grey-block"></div>
                     <h2 className="desc-text">Code in editor to complete task</h2>
-                    <img src={EditorInstruction} alt={"instruction that displays instructions"}style={{width: "600px", height: "348px", borderRadius: "22px"}}/>
+                    <img src={EditorInstruction} alt={"instruction that displays instructions"} className="width-half"/>
                 </div>
-                <div className="flex-start-block" style={{marginRight: "500px"}}>
+                <div className="flex-start-block margin-right">
                     <div className="grey-block"></div>
                     <h2 className="desc-text">Read description</h2>
-                    <video width="620" height="450" controls={false} autoPlay muted >
+                    <video className="width-half" controls={false} autoPlay muted >
                         <source src={InstructionsVideo} type="video/mp4"/>
                     </video>                
                 </div>
             </div>      
         <div className="learning-block flex-column">
-            <h1 className="header-text">Our coding  <span style={{color: "#b0b", fontFamily: "system-ui,-apple-system,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji"}}>Blocks</span></h1>
+            <h1 className="header-text">Our coding  <span className="pink-gradient ">Blocks</span></h1>
             <h2 className="subhead-text">We cover applying Text Modifiers, arranging views in Stack, and several types of Views</h2>
-            <div className="learning-blocks flex-row example-wrapper">
+            <div className="learning-blocks flex-example example-wrapper">
                 <div className="flex-column">                
                     <img className='landing-simulator-svg' src={ModifiersExampleIcon} alt="modifier similator example"/>
                     <p style={{fontSize: "1.5rem", fontWeight: "600", marginTop: "32px"}}>Modifier Block</p>
@@ -90,9 +90,9 @@ export const LandingPage = ({setlevel}) => {
         </div>
         <div className="advantage-wrapper flex-column">
             <div style={{width: "70%"}}>
-                <h1 className="header-text" style={{color: "rgb(84, 196, 188)"}}>We 😍</h1>
+                <h1 className="header-text" style={{color: "rgb(255, 37, 110)"}}>We 😍</h1>
                 <h1 className="header-text" style={{color: "white"}}>VS</h1> 
-                <h1 className="header-text" style={{color: "rgb(0, 113, 227)"}}>Articles, videos and courses</h1> 
+                <h1 className="header-text" style={{color: "rgb(255, 37, 37)"}}>Articles, videos and courses</h1> 
                 <h2 className="subhead-text" style={{color: "white", padding: "20px"}}>We are the next-generation platform for learning code by playing games that combines the missing parts of traditional learning: a tremendous amount of practice and an engaging learning environment.</h2>
             </div>
         </div>
